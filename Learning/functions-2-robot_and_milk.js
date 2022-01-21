701559a3cd8d0fb5b31ea7fc3c9c49214735cbf2
LuCaS <dumdumcz@gmail.com>
2022-01-21 14:16:09 -0800

    Update functions-2-robot_and_milk.js

--- Learning/functions-2-robot_and_milk.js
+++ Learning/functions-2-robot_and_milk.js
@@ -3,12 +3,14 @@ var bottles = prompt("How many milk bottles you want?");
 var money = prompt("How much $ you gave to robot?");
 var totalCost = bottles * price;
 var afford = money / price;
-var change = money - totalCost;
+var change = money - totalCost; 
+var change1 = totalCost % money; /*We can use a remainder here*/
 
 function getMilk(bottles) {   
   alert("OK, robot bought " + bottles + " of milk, and paid " + "$" + totalCost );
   alert("His change will be $" + change + " as return.");
-  alert("You requested to buy " + bottles + " ,but the robot could afford " + afford + " bottles!");
+  alert("Change using remainder will be " + change1);
+  alert("You requested to buy " + bottles + " ,but the robot could afford " + Math.floor(afford) + " bottles!");
 }
 
 getMilk(bottles);
