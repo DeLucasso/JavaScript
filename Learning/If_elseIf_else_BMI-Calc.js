c4f0589a8fbccf89a8785c7a65d0e565ac515c21
LuCaS <dumdumcz@gmail.com>
2022-01-26 20:39:08 -0800

    Create If_elseIf_else_BMI-Calc.js

---
+++ Learning/If_elseIf_else_BMI-Calc.js
@@ -0,0 +1,25 @@
+// JavaScript training with LuCaS, www.instagram.com/lukasnachtigall/
+// (c) LuCaS 2022. MIT License - Feel free to use and modify the code
+//
+// What does the code do?
+// Calculates BMI and returns 3 different messages based on the <bmi> variable value
+
+function bmiCalculator (weight, height) {
+    var bmi = Math.round(weight / Math.pow(height, 2));
+    var interpretation = "";
+   
+    if (bmi < 18.5) {
+        interpretation = "Your BMI is " + bmi + ", so you are underweight."
+    }
+
+    else if (bmi >= 18.5 && bmi < 24.9) {
+        interpretation = "Your BMI is " + bmi + ", so you have a normal weight."
+        
+    } else {
+        interpretation = "Your BMI is " + bmi + ", so you are overweight."
+        
+    }
+        return interpretation
+}
+
+bmiCalculator(90,1.8)
