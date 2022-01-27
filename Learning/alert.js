fa8611a914df765685abf02019604ee984c8e054
LuCaS <dumdumcz@gmail.com>
2022-01-26 18:57:50 -0800

    Update alert.js

--- Learning/alert.js
+++ Learning/alert.js
@@ -1,1 +1,4 @@
+// JavaScript training with LuCaS, www.instagram.com/lukasnachtigall/
+// (c) LuCaS 2022. MIT License - Feel free to use and modify the code
+
 alert("For a popup window, you can use an 'alert' function");
