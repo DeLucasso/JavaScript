65a0ee69ad1ab24b8296a84c45d3fa42fe50dbc0
LuCaS <dumdumcz@gmail.com>
2022-01-26 19:02:23 -0800

    Create prompt.js

---
+++ Learning/prompt.js
@@ -0,0 +1,11 @@
+// JavaScript training with LuCaS, www.instagram.com/lukasnachtigall/
+// (c) LuCaS 2022. MIT License - Feel free to use and modify the code
+
+// What this code does?
+// Prompt an input to a user and stores it to a variable
+
+var first = prompt("What's your first name?");
+var last = prompt("What's your last name?");
+
+// Output the First and Last name as popoup
+alert ("Nice to meet you " + first + " " + last);
