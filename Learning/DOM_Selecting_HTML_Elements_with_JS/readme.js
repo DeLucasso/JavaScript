c413a0dca7fc0a003049d92cb813713ecd812b4f
LuCaS <dumdumcz@gmail.com>
2022-01-29 12:24:18 -0800

    Update readme.js

--- Learning/DOM_Selecting_HTML_Elements_with_JS/readme.js
+++ Learning/DOM_Selecting_HTML_Elements_with_JS/readme.js
@@ -1,6 +1,8 @@
 // Inside the Chrome, Inspect the page, click on the element
 // and select COPY > COPY JS Path , then you can use it your querySelector
 // to target n-th child element
+//
+// Try this code in the console, it will change "Third" Li element value.
 
 var myChange = document.querySelector("body > ul > li:nth-child(3)");
 myChange.innerHTML = ("Wow, it really works folks!");
