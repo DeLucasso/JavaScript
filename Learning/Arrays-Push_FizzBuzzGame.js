103fa0b01a17e2f65dff76687215dd3e0453e003
LuCaS <dumdumcz@gmail.com>
2022-01-28 22:29:23 -0800

    Update Arrays-Push_FizzBuzzGame.js

--- Learning/Arrays-Push_FizzBuzzGame.js
+++ Learning/Arrays-Push_FizzBuzzGame.js
@@ -7,30 +7,26 @@
 // Adds a number to array [output]. If the number is
 // divisible by 3 inserts "Fizz", if is divisible by 5
 // inserts "Buzz", if is divisible by 3 and 5, inserts "FizzBuzz"
+// We are using a while loop with condition "counter <= 100"
 
 var output = [];
 var counter = 1;
 
 function fizzBuzz() {
+
+    while (counter <= 100) {
     
-    if (counter % 3 === 0) {
-        output.push("Fizz");
-    }
-
-    else if (counter % 5 === 0) {
-        output.push("Buzz");
-    }
-
     if (counter % 3 === 0 && counter % 5 === 0) {
         output.push("FizzBuzz");
-    }
-
-    else {
+    } else if (counter % 3 === 0) {
+        output.push("Fizz");
+    } else if (counter % 5 === 0) {
+        output.push("Buzz");
+    } else {
         output.push(counter);
-  
     }
-
-counter++;
-console.log (output);  
-
+    
+    counter++;
+    }
+    console.log (output);
 }
