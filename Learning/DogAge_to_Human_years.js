42d63c9d42ff4a457c08ea41407216f31d800d00
LuCaS <dumdumcz@gmail.com>
2022-01-28 20:48:24 -0800

    Update DogAge_to_Human_years.js

--- Learning/DogAge_to_Human_years.js
+++ Learning/DogAge_to_Human_years.js
@@ -1,3 +1,11 @@
+// JavaScript training with LuCaS, (c) LuCaS 2022
+// IG: @LukasNachtigall/ GitHub: github.com/DeLucasso
+// YT: youtube.com/lukasnachtigall
+// MIT License - Feel free to use and modify the code
+//
+// What does this code do?
+// Converts yours dog age to human years
+
 var dogAge = prompt("How old is you dog?");
 humanAge = ((dogAge - 2) * 4) + 21;
 alert("Your dog is " + humanAge + " years old in human years :)");
