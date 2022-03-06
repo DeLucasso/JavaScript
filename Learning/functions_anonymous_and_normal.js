0d1edf6a29684ae0a6a28fad688ce1315b92d1ff
LuCaS <dumdumcz@gmail.com>
2022-03-05 20:59:30 -0800

    Create functions_anonymous_and_normal.js

---
+++ Learning/functions_anonymous_and_normal.js
@@ -0,0 +1,11 @@
+// This is a normal function in JavaScript
+document.querySelector("button").addEventListener("click", clickAlert);
+
+function clickalert() {
+  alert("I got clicked!!");
+}
+
+// This is an anonymous function in JavaScript
+document.querySelector("button").addEventListener("click", function () {
+  alert("I got clicked!!");
+  )};
