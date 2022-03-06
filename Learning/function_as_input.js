732c1308f54bb76a9a8b8a1d1f09938b633e313e
LuCaS <dumdumcz@gmail.com>
2022-03-05 21:11:39 -0800

    Create function_as_input.js

---
+++ Learning/function_as_input.js
@@ -0,0 +1,14 @@
+// 1st function does adding two inputs
+function add (a, b) {
+    return a + b;
+}
+
+// 2nd function does multiply two inputs
+function multiply (a, b) {
+    return a * b;
+}
+
+// 3rd function take two numbers and function as inputs
+function calculator (a, b, operator) {
+    return operator (a,b);
+}
