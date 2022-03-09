62996a294856d8e38c723330c90bf125efc12b01
LuCaS <dumdumcz@gmail.com>
2022-03-08 20:33:59 -0800

    Update Creating_Constructor_Functions.js

--- Learning/Functions/Creating_Constructor_Functions.js
+++ Learning/Functions/Creating_Constructor_Functions.js
@@ -1,4 +1,5 @@
 // This is a Constructor Function, must start with CapitalLetter! :
+// "this" takes the input and assigns it to the parameter variable
 function HouseKeeper (name, years, age) {
   this.name = name;
   this.years = years;
