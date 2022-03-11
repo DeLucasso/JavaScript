a9dacbc3ed2f4c1b973e8d5daa4fac7980f06377
LuCaS <dumdumcz@gmail.com>
2022-03-10 20:21:41 -0800

    Add files via upload

---
+++ Learning/Drums_Complete_Page/index.js
@@ -0,0 +1,82 @@
+
+
+// Detecting Button Press (mouse)
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+    currentKey (buttonInnerHTML);
+
+  });
+}
+
+
+// Detecting Keyboard Press
+
+document.addEventListener("keydown", function (event) {
+
+  makeSound(event.key);
+  currentKey(event.key);
+
+   });
+
+
+function makeSound (key) {
+
+  switch (key) {
+
+    case "w":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "a":
+      var kickbass = new Audio('sounds/kick-bass.mp3');
+      kickbass.play();
+      break;
+
+    case "s":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "d":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+
+    case "j":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+
+    case "k":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    default: console.log()
+  }
+}
+
+function currentKey(keyPressed) {
+  
+  var activeKey = document.querySelector("." + keyPressed);
+  activeKey.classList.add("pressed");
+
+    setTimeout( function() {
+    activeKey.classList.remove("pressed");
+}, 50);
+
+}
