b378eb471a0fd0e5d07b378ce5bcdceb64f8833d
LuCaS <dumdumcz@gmail.com>
2022-05-08 23:19:44 -0700

    Update switch.js

--- Learning/switch.js
+++ Learning/switch.js
@@ -1,5 +1,6 @@
 // The Switch statement compares expression to each case. If no case is equal to expression, then <default> clause is executed.
 // Difference between IF ELSE and SWITCH is, that IF ELSE evaluates expression and logic
+// The rule of thumb: If you have more than 5 IF ELSE statements, you should use <switch> statement
 
 var expression = "Lemonade";
 
